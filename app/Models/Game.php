c0de95271893377ff61e17939a4deac554141152
Trung Hieu <hiutrum@gmail.com>
2026-04-08 11:08:22 +0700

    add model relationships

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function keys() {
+        return $this->hasMany(GameKey::class);
+    }
 }

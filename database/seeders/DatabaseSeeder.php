885a0934db450e128924868be7cac5d54c63149b
Trung Hieu <hiutrum@gmail.com>
2026-04-08 16:48:16 +0700

    fix backend logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
 
-        $this->call(GameSeeder::class);
+        $this->call([
+            GameSeeder::class,
+            GameKeySeeder::class,
+        ]);
     }
 }

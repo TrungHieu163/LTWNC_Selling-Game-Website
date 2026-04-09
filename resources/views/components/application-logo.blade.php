dfb1b8588e4e4498160e43e12a1a451dff7a76cd
huutheng <thanhbingo5002@gmail.com>
2026-04-09 16:47:54 +0700

    Add favicon

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img src="{{ asset('images/logo.jpg') }}" {{ $attributes->merge(['class' => 'w-20 h-20 rounded-full object-cover']) }}
-    alt="Logo Game">
+<img src="{{ asset('images/Mixigaming-Logo.jpg') }}"
+    {{ $attributes->merge(['class' => 'w-20 h-20 rounded-full object-cover']) }} alt="Logo Game">

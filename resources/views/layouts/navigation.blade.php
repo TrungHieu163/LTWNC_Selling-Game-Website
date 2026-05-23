5fc34d9dd07642c06ad81a1b50c42cd37a83185f
Trung Hieu <hiutrum@gmail.com>
2026-05-23 22:52:20 +0700

    add auth for guest

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -40,6 +40,7 @@
             </div>
 
             <!-- Settings Dropdown -->
+            @auth
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -75,6 +76,19 @@
                     </x-slot>
                 </x-dropdown>
             </div>
+            @endauth
+
+            @guest
+            <div class="flex items-center gap-4">
+                    <a href="{{ route('login') }}" class="text-sm text-gray-300 hover:text-white px-4 py-2 transition">
+                        Đăng nhập
+                    </a>
+                    <a href="{{ route('register') }}"
+                        class="text-sm bg-white text-black hover:bg-gray-100 px-5 py-2 rounded-md font-medium transition">
+                        Đăng ký
+                    </a>
+                </div>
+            @endguest
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -101,6 +115,7 @@
         </div>
 
         <!-- Responsive Settings Options -->
+        @auth
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
@@ -123,5 +138,6 @@
                 </form>
             </div>
         </div>
+        @endauth
     </div>
 </nav>
